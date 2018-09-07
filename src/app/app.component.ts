52474781bec8ef998bd45ddce3eb41e446a671be
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-09-07 10:52:56 +1000

    Pullied in product images and added to app for display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-inventory-app';
+  product: Product[];
+
+  constructor() {
+    this.product = [
+      new Product(
+        'BLACKCAP',
+        'A Black Baseball Cap',
+        '/assets/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99,
+      ),
+      new Product(
+        'BLACKSHOE',
+        'Nike Sneakers - Black',
+        '/assets/images/products/black-shoes.jpg',
+        ['Men', 'Footwear', 'Sneakers'],
+        249.95,
+      ),
+      new Product(
+        'BLUEJACKET',
+        'A Blue Windbreaker',
+        '/assets/images/products/blue-jacket.jpg',
+        ['Men', 'Apparel', 'Jackets & Vests'],
+        89.95,
+      ),
+    ];
+  }
 }
