af41a71b3b3f3e2e6895b89369e0ea55d1979795
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-09-07 13:41:48 +1000

    Creating ProductRow + sub components to display productList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [AppComponent, ProductListComponent, ProductRowComponent, ProductImageComponent, ProductDepartmentComponent, PriceDisplayComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
